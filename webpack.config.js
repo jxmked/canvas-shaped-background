039ea23a4fdc7d98f0b3ffd8126fcc3e6214b412
Jovan <jovandeguia@gmail.com>
2023-01-13 14:20:55 +0800

    Not collision related pr (#4)
    Note: Not Collision Related 
    
    * fixing interfaces
    
    * lint required
    
    * update
    
    * 1.0.1
    
    * 1.0.2
    
    * 1.0.3
    
    * v1.0.3: linted
    
    * Prettier added
    
    * update
    
    * Added action
    
    * 1.0.4

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,67 @@ const path = require('path');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 var devMode = process.env['NODE' + '_ENV'] !== 'production'
+const CONFIG = {
+    
+    output: {
+        name: "[name].[contenthash]",
+        chunk: "[id].[contenthash]",
+        dir: "dist" // Do not include './' or '/' 
+    },
+    input: {
+        entry: './src/index.ts', // Typescript only
+        dir: 'src'
+    },
+    
+    // Manifesting and information
+    
+    // For site.webmanifest
+    appName: 'Canvas Shape Background',
+    shortAppName: 'Shapes',
+    description: 'Canvas animation',
+    colors: {
+        background: '#3a3a3c',
+        theme: '#272738' // also injected into html
+    },
+    favicon: 'favicon.ico', // Must be ends with .ico 
+    icons: {
+        src: path.resolve('src/assets/icon.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+    },
+}
 
 module.exports = function(env, config) {
     
-    if(process.env['NODE' + '_ENV'] === void 0)
+    if(process.env['NODE' + '_ENV'] === void 0) {
+        // From flag '--mode'
         devMode = config.mode !== "production";
+    }
     
     console.log("DEV MODE: " + String(devMode) + "\n")
     
+    if(devMode) {
+        CONFIG.output.name = "[name]";
+        CONFIG.output.chunk = "[id]";
+    }
+    
     return {
-        entry: './src/index.ts',
+        entry: CONFIG.input.entry,
         module: {
             rules: [{
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
             }, {
-                test: /\.(jpe?g|png|gif|svg)$/i,
+                test: /\.(jpe?g|png|gif|svg|webp|webg)$/i,
                 loader: "file-loader"
             }, {
                 test: /\.(woff|ttf|otf|eot|woff2)$/i,
+                loader: "file-loader"
+            }, {
+                test: /\.(wav|mp3|mp4|avi)$/i,
                 loader: "file-loader"
             }, {
                 test: /\.((s[ca]|c)ss)$/,
@@ -42,9 +81,9 @@ module.exports = function(env, config) {
         },
         
         output: {
-            filename: devMode ? "[name].js" : "[name].[contenthash].js",
-            chunkFilename: devMode ? "[id].js" : "[id].[contenthash].js",
-            path: path.resolve(__dirname, './dist'),
+            filename:  CONFIG.output.name + ".js",
+            chunkFilename: CONFIG.output.chunk + ".js",
+            path: path.resolve(__dirname, './' + CONFIG.output.dir),
             clean: true
         },
         
@@ -59,23 +98,59 @@ module.exports = function(env, config) {
                 host: 'localhost',
                 port: 3000,
                 server: {
-                    baseDir: ['dist']
+                    baseDir: [CONFIG.output.dir]
                 },
-                files: ['./dist/*'],
-                notify: false,
-                ui: false,
-                open:false
+
+                files: ['./' + CONFIG.output.dir + '/*'],
+                notify: false, 
+                ui: false, // Web UI for BrowserSyncPlugin
+                open:false // Open browser after initiation
+
             }),
                 
             new HtmlWebpackPlugin({
                 filename: 'index.html',
-                favicon: './src/favicon.ico',
-                template: './src/index.html'
+                favicon: './' + path.join(CONFIG.input.dir, CONFIG.favicon),
+                template: './' + path.join(CONFIG.input.dir, 'index.html'),
+             //   manifest: './src/site.webmanifest',
+                showErrors: devMode, // Include html error on emitted file
+                meta: {
+                    viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+                    robots: 'index,follow',
+                    referrer: 'origin',
+                    charset: { charset: 'UTF-8' },
+                    'http-equiv': {
+                        'http-equiv': 'Content-Type',
+                        content: 'text/html; charset=UTF-8'
+                    },
+                    'color-scheme': 'light dark'
+                }
             }),
             
             new MiniCssExtractPlugin({
-                filename: devMode ? "[name].css" : "[name].[contenthash].css",
-                chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css",
+                filename: CONFIG.output.name + ".css",
+                chunkFilename: CONFIG.output.chunk + ".css",
+            }),
+            
+            new WebpackPwaManifest({
+                name: CONFIG.appName,
+                short_name: CONFIG.shortAppName,
+                description: CONFIG.description,
+                orientation: "portrait",
+                start_url: "/",
+                background_color: CONFIG.colors.background,
+                theme_color: CONFIG.colors.theme,
+                icons: [{
+                    src: CONFIG.icons.src,
+                    sizes: CONFIG.icons.sizes,
+                    purpose: "any maskable"
+                }],
+                
+                // Asset config
+                fingerprints: false, // Remove hashed in filename
+                publicPath: './', // Make sure the url starts with 
+                inject: true, // Insert html tag <link rel="manifest" ... />
+                filename: 'site.webmanifest'
             }),
         ]
     }
